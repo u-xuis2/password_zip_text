bdb28ff5ce1364b2fc4190b0c9b77a29441ab2f3
u.xuis2 <u.xuis2nidxmum@gmail.com>
2025-07-15 13:15:11 +0900

    サブディレクトリ /password_zip_text/ での動作に対応
    - サーバー側の静的ファイル配信パスを /password_zip_text に変更
    - Reactアプリのhomepageをサブディレクトリパスに設定
    - HTML内のog:urlとcanonical URLを適切に設定

--- server/index.js
+++ server/index.js
@@ -31,9 +31,9 @@ app.use(express.urlencoded({ extended: true, limit: '1mb' }));
 
 // 静的ファイル配信（本番環境用）
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/build')));
+    app.use('/password_zip_text', express.static(path.join(__dirname, '../client/build')));
     
-    app.get('*', (req, res) => {
+    app.get('/password_zip_text/*', (req, res) => {
         res.sendFile(path.join(__dirname, '../client/build/index.html'));
     });
 }
